aa3c8a3702cc292d82d77f4935be64cb33b2c28a
davmein0 <144638896+davmein0@users.noreply.github.com>
2026-03-21 22:41:13 -0400

    Progress, made a superuser, added post, /blog/ progress on the template

--- homework06/homework6/blog/views.py
+++ homework06/homework6/blog/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from .models import Post
+from django.views.generic import ListView, DetailView
 
 # Create your views here.
 def index(request):
-    
+    context = {"Latest Posts": Post.objects.order_by("-pub_date")[:5]}
+    return render(request, "blog/index.html", context)
+
+class IndexView(ListView):
+    template_name = "blog/index.html"
+    context_object_name = "latest_posts"
+
+    def get_queryset(self):
+        """Return the last five posts."""
+        return Post.objects.order_by("-pub_date")[:5]
